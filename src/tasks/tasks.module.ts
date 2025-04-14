00399404ccc50adfedcb0c364452d60be95d0a08
omarbougarne <omarbougarne98@gmail.com>
2025-04-14 17:15:52 +0100

    Feat adding model to task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TasksService } from './tasks.service';
+import { Task, TaskSchema } from './schemas/task.schema';
+import { TasksController } from './tasks.controller';
 
 @Module({
-  providers: [TasksService]
+  imports: [
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }])
+  ],
+  controllers: [TasksController],
+  providers: [TasksService],
+  exports: [TasksService],
 })
-export class TasksModule {}
+export class TasksModule { }
