f7ecf69c1904ca22b54f074cd372393b2f96f9b6
omarbougarne <omarbougarne98@gmail.com>
2025-04-16 11:22:59 +0100

    Feat Service for logic of creation of projects based on already made code just few update

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ProjectsService } from './projects.service';
 
-@Module({})
+@Module({
+  providers: [ProjectsService]
+})
 export class ProjectsModule {}
